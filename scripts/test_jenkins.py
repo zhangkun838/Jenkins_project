6f36aaf0165848e4c0464c732e52963e9dcd8ed0
zhangkun838 <67966197+zhangkun838@users.noreply.github.com>
2020-11-29 21:48:34 +0800

    update code

--- scripts/test_jenkins.py
+++ scripts/test_jenkins.py
@@ -8,10 +8,10 @@ class TestJenkins:
 
     @allure.severity("critical")
     def test_02(self):
-        allure.attach(body="jeitu", name="test_02的文字描述：", attachment_type=allure.attachment_type.TEXT)
-        assert 0
-        allure.attach(body=self.driver.get_screenshot_as_png(), name="test_02的截图：",
-                      attachment_type=allure.attachment_type.PNG)
+        assert 1
 
     def test_03(self):
         assert 1
+
+if __name__ == '__main__':
+    print(".....")
